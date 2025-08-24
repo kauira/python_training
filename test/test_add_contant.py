47d8a71a1fc883521507e5e5a415b127db2b7e71
Ira <kauira@yandex.ru>
2025-08-24 19:49:44 +0300

    Выделен класс для работы с контактами

--- test/test_add_contant.py
+++ test/test_add_contant.py
@@ -12,7 +12,7 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contant(Contant(firstname="ggg", middlename="fgh",lastname="dfg", nickname="rgrgrg", title="dfgd", company="erferfer", address="dfgdgdd", home="dfgdfg", mobile="dgdgdgd",
+    app.contact.create(Contant(firstname="ggg", middlename="fgh",lastname="dfg", nickname="rgrgrg", title="dfgd", company="erferfer", address="dfgdgdd", home="dfgdfg", mobile="dgdgdgd",
                                 work="dgdgdg", fax="dfgdgdf", email="dfgvdfgfd", email2="dfgdgfgd", email3="dgdgd", homepage="dgdgdg", bday="10", bmonth="September", byear="111",
                             aday="1111", amonth="August", ayear="15"))
     app.session.logout()
