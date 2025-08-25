2478e824b96552bf680534b7762daee5731f4a46
Ira <kauira@yandex.ru>
2025-08-25 20:43:02 +0300

    Добавлен тест на редактирование первой группы

---
+++ test/test_edit_first_group.py
@@ -0,0 +1,6 @@
+from model.group import Group
+
+def test_add_group(app):
+    app.session.login( username="admin", password="secret")
+    app.group.edit_first_group(Group(name="123", header="444", footer="555"))
+    app.session.logout()
