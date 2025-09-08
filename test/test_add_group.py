3bbefbdf9008f2c543d92216ee06ed9e1c30ca87
Ira <kauira@yandex.ru>
2025-09-08 21:38:00 +0300

    Метод получения списка групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,9 +1,15 @@
 from model.group import Group
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create_group(Group(name="gg", header="gggg", footer="gggggg"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create_group(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,9 +3,16 @@ from model.group import Group
 def test_edit_group_name(app):
     if app.group.count() == 0:
         app.group.create_group(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="New group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+
 
 def test_edit_group_header(app):
     if app.group.count() == 0:
         app.group.create_group(Group(header="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
