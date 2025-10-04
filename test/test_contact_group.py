bec222b164e6732974e87f4b3873f7bf07463bfb
Ira <kauira@yandex.ru>
2025-10-04 23:35:11 +0300

    Исправление теста добавления контакта в группу

--- test/test_contact_group.py
+++ test/test_contact_group.py
@@ -6,13 +6,15 @@ db = ORMFixture(host='127.0.0.1',name='addressbook',user='root',password='')
 
 def test_add_contact_to_group(app):
     contacts = app.contact.get_contact_list()
-    groups = db.get_group_list()
+    groups = app.group.get_group_list()
     contact = random.choice(contacts)
     group = random.choice(groups)
     group_contacts = app.contact.get_contacts_in_group(group.id)
-    if contact not in group_contacts:
-        app.contact.add_contact_to_group(contact.id,group.id)
-        assert sorted(group_contacts, key=Contact.id_or_max) == sorted(db.get_contacts_in_group(group), key=Contact.id_or_max)
+    if contact in group_contacts:
+        app.contact.remove_contact_from_group(contact.id, group.id, group.name)
+    app.contact.add_contact_to_group(contact.id,group.id)
+    group_contacts_after_add = app.contact.get_contacts_in_group(group.id)
+    assert sorted(group_contacts_after_add, key=Contact.id_or_max) == sorted(db.get_contacts_in_group(group), key=Contact.id_or_max)
 
 def test_remove_contact_from_group(app):
     contacts = app.contact.get_contact_list()
