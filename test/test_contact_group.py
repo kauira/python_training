47019a01aabbd94cdeb46d238894d46986f40930
Ira <kauira@yandex.ru>
2025-10-05 00:08:09 +0300

    Исправление теста добавления контакта в группу 2

--- test/test_contact_group.py
+++ test/test_contact_group.py
@@ -13,8 +13,7 @@ def test_add_contact_to_group(app):
     if contact in group_contacts:
         app.contact.remove_contact_from_group(contact.id, group.id, group.name)
     app.contact.add_contact_to_group(contact.id,group.id)
-    group_contacts_after_add = app.contact.get_contacts_in_group(group.id)
-    assert sorted(group_contacts_after_add, key=Contact.id_or_max) == sorted(db.get_contacts_in_group(group), key=Contact.id_or_max)
+    assert sorted(group_contacts, key=Contact.id_or_max) == sorted(db.get_contacts_in_group(group), key=Contact.id_or_max)
 
 def test_remove_contact_from_group(app):
     contacts = app.contact.get_contact_list()
